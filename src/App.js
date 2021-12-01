413f2d8d081e36204d7daf2ae764c23344c0021e
satyadvaya <satyadvaya@gmail.com>
2021-11-30 18:04:49 -0800

    added images and components, and hopefully fixed eslint nonsense for good

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Character from './components/Character/Character'
+import Display from './components/Display/Display'
+import Picker from './components/Picker/Picker'
+import { useState } from 'react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <header className="App-header">
+          <h1>Frankenstein Laboratory</h1>
+          <Picker />
+          <Display />
+          <Character />
+        </header>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Picker/Picker.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Picker() {
+  return <h1>Picker</h1>
+}
